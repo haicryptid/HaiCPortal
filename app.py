04865a1578b1b3391c241d63f61e87da45f438a5
HaiCryptid <hischool030@gmail.com>
2025-06-28 12:34:26 +0900

    host 0.0.0.0 추가함

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def dashboard():
     return render_template("dashboard.html", logs=logs)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
